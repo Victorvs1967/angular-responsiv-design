e5680b849e8c14ac401809d3fe7a4493c06dfec3
Victorvs1967 <victorsmirnov@me.com>
2022-12-23 23:00:20 +0200

    service and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ResponsiveService } from './responsive/responsive.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-responsiv-design';
+  constructor(public responsiveService: ResponsiveService) {}
 }
--- src/app/responsive/responsive.service.ts
+++ src/app/responsive/responsive.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { ScreenSizeType } from './responsive.enum';
+import { BreakpointType, DeviceType, OrientationType, ScreenSizeType } from './responsive.enum';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +8,8 @@ import { ScreenSizeType } from './responsive.enum';
 export class ResponsiveService {
 
   constructor(breakpointObserver: BreakpointObserver) {
-
+    this.checkScreenSize(breakpointObserver);
+    this.checkDeviceTypeAndOrientation(breakpointObserver);
   }
 
   private _screenSize = ScreenSizeType.Unknown;
@@ -24,7 +25,95 @@ export class ResponsiveService {
     [Breakpoints.XLarge, ScreenSizeType.XLarge],
   ]);
 
-  private checkScreenSize(): void {
-    
+  private checkScreenSize(breakpointObserver: BreakpointObserver): void {
+    breakpointObserver
+      .observe([
+        Breakpoints.XSmall,
+        Breakpoints.Small,
+        Breakpoints.Medium,
+        Breakpoints.Large,
+        Breakpoints.XLarge,
+      ])
+      .subscribe((result: any) => {
+        for (const query of Object.keys(result.breakpoints)) {
+        if (result.breakpoints[query]) {
+          this._screenSize = this.screenSizeBreakpoints.get(query) ?? ScreenSizeType.Unknown;
+        }
+      }
+    });
   }
+
+  public orientationPortrait(): boolean {
+    return this._orientation === OrientationType.Portrait;
+  }
+
+  public orientationLandscape(): boolean {
+    return this._orientation === OrientationType.Landscape;
+  }
+
+  public deviceDesktop(): boolean {
+    return this,this._deviceType === DeviceType.Web;
+  }
+
+  public deviceTablet(): boolean {
+    return this._deviceType === DeviceType.Tablet;
+  }
+
+  public deviceMobile(): boolean {
+    return this._deviceType === DeviceType.Handset;
+  }
+
+  private _orientation = OrientationType.Unknown;
+  public get orientation(): OrientationType {
+    return this._orientation;
+  }
+
+  private _deviceType = DeviceType.Unknown;
+  public get deviceType(): DeviceType {
+    return this._deviceType;
+  }
+
+  private readonly deviceAndOrientation = new Map([
+    [Breakpoints.HandsetLandscape, BreakpointType.HandsetLandscape],
+    [Breakpoints.HandsetPortrait, BreakpointType.HandsetPortrait],
+    [Breakpoints.TabletLandscape, BreakpointType.TabletLandscape],
+    [Breakpoints.TabletPortrait, BreakpointType.TabletPortrait],
+    [Breakpoints.WebLandscape, BreakpointType.WebLandscape],
+    [Breakpoints.WebPortrait, BreakpointType.WebPortrait],
+  ]);
+
+  private checkDeviceTypeAndOrientation(breakpointObserver: BreakpointObserver): void {
+    breakpointObserver
+    .observe([
+      Breakpoints.HandsetPortrait,
+      Breakpoints.HandsetLandscape,
+      Breakpoints.WebPortrait,
+      Breakpoints.WebLandscape,
+      Breakpoints.TabletPortrait,
+      Breakpoints.TabletLandscape,
+    ])
+    .subscribe((result: any) => {
+      let orientationTypes = Object.keys(OrientationType).map(key => key);
+      let deviceTypes = Object.keys(DeviceType).map(key => key);
+
+      for (const query of Object.keys(result.breakpoints)) {
+        if (result.breakpoints[query]) {
+          let type = this.deviceAndOrientation.get(query) ?? BreakpointType.Unknown;
+
+          orientationTypes.forEach(element => {
+            if (type.indexOf(element) !== -1) {
+              this._orientation = element as OrientationType;
+            }
+          });
+
+          deviceTypes.forEach(element => {
+            if (type.indexOf(element) !== -1) {
+              this._deviceType = element as DeviceType;
+            }
+          });
+        }
+      }
+    });
+  }
+
 }
